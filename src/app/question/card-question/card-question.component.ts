1348aed82fddd66e2dbd669a6c1ec3c2f2998fad
Anthony Monsalve <22anthony.monsalve@gmail.com>
2025-02-20 16:57:37 -0400

    trivia estructura

---
+++ src/app/question/card-question/card-question.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-question',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-question.component.html',
+  styleUrl: './card-question.component.css'
+})
+export class CardQuestionComponent {
+
+}
---
+++ src/app/question/option-question/option-question.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-option-question',
+  standalone: true,
+  imports: [],
+  templateUrl: './option-question.component.html',
+  styleUrl: './option-question.component.css'
+})
+export class OptionQuestionComponent {
+
+}
