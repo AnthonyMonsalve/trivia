4c2342c144b0b304f2ee48ed9ecd6459e2133f52
AnthonyMonsalve <22anthony.monsalve@gmail.com>
2025-02-20 21:38:46 -0400

    card trivia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CardQuestionComponent } from "./question/card-question/card-question.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, CardQuestionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
